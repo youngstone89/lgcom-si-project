d3992381ff5552b51c57c94cdd390e0b3cd66729
youngstone <youngstone@lgcns.com>
2017-07-03 18:12:53 +0900

    Added IPUser Class inheriting django.contrib.auth.models.User

--- lgcom/models.py
+++ lgcom/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 
 
 # Create your models here.
@@ -11,4 +11,21 @@ class IPAddress(models.Model):
 	def __str__(self):
 		return self.ip_address
 
-		
+
+class IPUser(User):
+	
+	SI = 'SI'
+	SM = 'SM'
+
+	USER_GROUP_CHOICES = ((SI,SI),(SM,SM),)
+
+	USER_GROUP = models.CharField(max_length=2,choices=USER_GROUP_CHOICES,default=SI)
+
+	HUMAN = 'HUMAN'
+	DEVICE = 'DEVICE'
+
+	USER_TYPE_CHOICES = ((HUMAN,HUMAN),(DEVICE,DEVICE),)
+	USER_TYPE = models.CharField(max_length=5,choices=USER_TYPE_CHOICES,default=HUMAN)
+
+	def __str__(self):
+		return 'IP User Info: {0},{1},{2}'.format(self.username, self.USER_TYPE, self.USER_GROUP)
